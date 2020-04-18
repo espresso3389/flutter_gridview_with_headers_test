2aea0ddf78e0931faf9935f58bceef18181e5f1c
Takashi Kawasaki <espresso3389@gmail.com>
2020-04-19 00:12:03 +0900

    GridView with Header.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_sticky_header/flutter_sticky_header.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,107 +13,125 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  ScrollController controller;
+  SliverGridLayout layout;
+  double headerSize = 60;
+  final itemCounts = [40, 20, 40, 12, 30, 120];
+  List<int> itemOffsets;
+  int dest = 1;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    controller = ScrollController();
+    int sum = 0;
+    itemOffsets = [];
+    for (int i = 0; i < itemCounts.length; i++) {
+      itemOffsets.add(sum);
+      sum += itemCounts[i];
+    }
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('GridView Test'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: CustomScrollView(
+        controller: controller,
+        slivers: <Widget>[
+          for (var i = 0; i < itemCounts.length; i++)
+            ...[
+              SliverStickyHeader(
+                header: Container(
+                  height: headerSize,
+                  color: Colors.lightBlue,
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Header #${i + 1} (${itemOffsets[i] + 1} - ${itemOffsets[i] + itemCounts[i]})',
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+                sliver: SliverGrid(
+                  gridDelegate: SliverGridDelegateInterceptor(
+                    gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                      maxCrossAxisExtent: 150.0,
+                      mainAxisSpacing: 8.0,
+                      crossAxisSpacing: 8.0,
+                      childAspectRatio: 1.0,
+                    ),
+                    onNewLayout: (layout) {
+                      this.layout = layout;
+                    }
+                  ),
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      return Container(
+                        color: Colors.primaries[index % Colors.primaries.length], height: 30,
+                        child: Text('${itemOffsets[i] + index + 1}'));
+                    },
+                    childCount: itemCounts[i]
+                  )
+                ),
+              ),
+            ]
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () {
+          final offset = itemCounts.take(dest).fold(0, (s, a) => s + layout.computeMaxScrollOffset(a)) + dest * headerSize;
+          dest = (dest + 1) % itemCounts.length;
+          controller.animateTo(offset, duration: Duration(milliseconds: 500), curve: Curves.easeInOut);
+        },
+        tooltip: 'Test',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
+}
+
+
+class SliverGridDelegateInterceptor extends SliverGridDelegateWithMaxCrossAxisExtent {
+  final SliverGridDelegateWithMaxCrossAxisExtent gridDelegate;
+  final void Function(SliverGridLayout newLayout) onNewLayout;
+  SliverGridDelegateInterceptor({this.gridDelegate, this.onNewLayout}):
+    super(
+      maxCrossAxisExtent: gridDelegate.maxCrossAxisExtent,
+      mainAxisSpacing: gridDelegate.mainAxisSpacing,
+      crossAxisSpacing: gridDelegate.crossAxisSpacing,
+      childAspectRatio: gridDelegate.childAspectRatio);
+
+  @override
+  SliverGridLayout getLayout(SliverConstraints constraints) {
+    final layout = gridDelegate.getLayout(constraints);
+    onNewLayout?.call(layout);
+    return layout;
+  }
+
+  @override
+  bool shouldRelayout(SliverGridDelegate oldDelegate) => gridDelegate.shouldRelayout(oldDelegate as SliverGridDelegateWithMaxCrossAxisExtent);
 }
